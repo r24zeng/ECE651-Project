0542e871f1f70351e706257b10415b397ec26000
QuanyuWANG96 <quanyuwang96@gmail.com>
2020-02-19 16:24:16 -0500

    add fragment xml and class for new

---
+++ NewsApp/app/src/main/java/ca/uwaterloo/newsapp/ui/news/fg_search.java
@@ -0,0 +1,22 @@
+package ca.uwaterloo.newsapp.ui.news;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ca.uwaterloo.newsapp.R;
+
+public class fg_search extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fg_search, container, false);
+        return view;
+    }
+
+}
