283bbdd00d963fa9e0a88dce8e08a9f9b8fb16cd
QuanyuWANG96 <quanyuwang96@gmail.com>
2020-03-25 14:58:17 -0400

    implement search and add to favorite

--- NewsApp/app/src/main/java/ca/uwaterloo/newsapp/ui/news/FavoriteFragment.java
+++ NewsApp/app/src/main/java/ca/uwaterloo/newsapp/ui/news/FavoriteFragment.java
@@ -1,22 +1,79 @@
-package ca.uwaterloo.newsapp.ui.news;
 
+package ca.uwaterloo.newsapp.ui.news;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import ca.uwaterloo.newsapp.R;
 
-public class FavoriteFragment extends Fragment {
+
+import java.util.ArrayList;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+import ca.uwaterloo.newsapp.Entity.Category;
+import ca.uwaterloo.newsapp.Entity.User;
+import ca.uwaterloo.newsapp.R;
+import ca.uwaterloo.newsapp.utils.ACache;
+import ca.uwaterloo.newsapp.utils.HttpUtils;
+import ca.uwaterloo.newsapp.utils.RecycleFavoriteAdapter;
+
+public class FavoriteFragment extends Fragment{
+    private static String TAG = "FavoriteFragment";
+    private Map<Integer,String> mStrs = new HashMap<>();
+    public RecyclerView _recycler;
+    User user;
+    RecycleFavoriteAdapter _recyclerAdapter;
+    private List<String> favorite_copy = new ArrayList<>();
+
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fg_favorite, container, false);
+        getList(mStrs);
+
+        String token = ACache.get(this.getActivity()).getAsString("token");
+        int id = (int)ACache.get(this.getActivity()).getAsObject("id");
+        HttpUtils httpUtils = new HttpUtils();
+        favorite_copy.clear();
+
+        user = httpUtils.getUser2("/api/v1/users/"+id,token);
+        String[] strings = user.getFollowing().split(",");
+        for (String s:strings){
+//            System.out.println(TAG + s + "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% "+ mStrs.get(Integer.valueOf(s)));
+            favorite_copy.add(mStrs.get(Integer.valueOf(s)));
+        }
+        _recycler = (RecyclerView)view.findViewById(R.id.recyclerSearch);
+        _recyclerAdapter = new RecycleFavoriteAdapter(favorite_copy, getActivity(),1);
+        _recycler.setAdapter(_recyclerAdapter);
+        _recyclerAdapter.notifyDataSetChanged();
+//        updateData(favorite_copy);
+        _recycler.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL,false));
+        _recycler.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL));
+        _recyclerAdapter.notifyDataSetChanged();
         return view;
     }
 
+    public void getList(Map<Integer,String> str){
+        HttpUtils httpUtils = new HttpUtils();
+        List<Category> list = httpUtils.getNewsCategory("/api/v1/news-source");
+        for (Category category:list){
+            str.put(category.getId(),category.getName());
+        }
+    }
+    
 }
+
---
+++ NewsApp/app/src/main/java/ca/uwaterloo/newsapp/utils/RecyclerSearchAdapter.java
@@ -0,0 +1,126 @@
+package ca.uwaterloo.newsapp.utils;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import ca.uwaterloo.newsapp.Entity.Category;
+import ca.uwaterloo.newsapp.Entity.User;
+import ca.uwaterloo.newsapp.R;
+
+public class RecyclerSearchAdapter extends RecyclerView.Adapter<RecyclerSearchAdapter.mySViewHolder>{
+    private Context context;
+    private List<Category> attribute;
+    private int check;
+    private Map<Integer, Boolean> map = new HashMap<>();
+    String token;
+    int id;
+
+
+    public RecyclerSearchAdapter(List<Category> attribute, Context context , int check, String token, int id){
+        this.context = context;
+        this.attribute = attribute;
+        this.check = check;
+        this.token = token;
+        this.id = id;
+        initMap();
+    }
+
+    public void initMap(){
+        for (int i = 0; i<attribute.size(); i++){
+            map.put(i,false);
+        }
+    }
+
+    @NonNull
+    @Override
+    public RecyclerSearchAdapter.mySViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = View.inflate(context, R.layout.item_search, null);
+        return new mySViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerSearchAdapter.mySViewHolder holder, int position) {
+        String attrinfo = attribute.get(position).getName();
+//        System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"+ attrinfo);
+        TextView textview = holder._allNews;
+        textview.setText(attrinfo);
+
+        holder._favourite_icon.setTag(position);
+        holder._favourite_icon.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                int pos = (int) buttonView.getTag();
+                if (isChecked) {
+                    map.put(pos, true);
+                    HttpUtils httpUtils = new HttpUtils();
+                    List<Category> list = httpUtils.getNewsCategory("/api/v1/news-source");
+                    for (Category category:list){
+                        if(category.getName().equals(attrinfo)){
+                            User newU = httpUtils.get("/api/v1/users/" + id, token) ;
+                            String[] strings = newU.getFollowing().split(",");
+                            int ifExists = 0;
+                            for(int i = 0; i< strings.length; i++){
+                                if(category.getId() == i){
+                                    ifExists = 1;
+                                    break;
+                                }
+                            }
+                            if(ifExists ==0){
+                                newU.setFollowing(newU.getFollowing() + "," + String.valueOf(category.getId()));
+                            }
+
+                            try {
+                                ResponseBody response = new HttpUtils().patch("/api/v1/users/"+id, JsonUtil.toJson(newU),token);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            }
+        });
+
+        if(map.get(position) == null){
+            map.put(position,false);
+        }
+        holder._favourite_icon.setChecked(map.get(position));
+    }
+
+
+
+    @Override
+    public int getItemCount() {
+        return attribute.size();
+    }
+
+    public void setFilter(List<Category>filterWords){
+        attribute=filterWords;
+        notifyDataSetChanged();
+    }
+
+
+
+    class mySViewHolder extends RecyclerView.ViewHolder {
+        private TextView _allNews;
+        private CheckBox _favourite_icon;
+
+
+        public mySViewHolder(View itemView) {
+            super(itemView);
+            _allNews = (TextView) itemView.findViewById(R.id.allNews);
+            _favourite_icon = (CheckBox) itemView.findViewById(R.id.favourite_icon);
+            this.setIsRecyclable(false);
+        }
+    }
+}
